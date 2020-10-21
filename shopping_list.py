02781d0486887ee599eb6424b0a11f865b5dfaf4
Agata Martyna <agatamartynachalupka@gmail.com>
2020-10-21 22:23:19 +0200

    Add loop

--- shopping_list.py
+++ shopping_list.py
@@ -3,3 +3,5 @@ shop_list = {
     'mięsny':['jagnięcina', 'krakowska sucha', 'skrzydełka'],
     'kiosk':['gazeta', 'baterie', 'zapałki']
 }
+for item in shop_list:
+    print(f"Idę do {item} i kupuję {shop_list[item]}.")

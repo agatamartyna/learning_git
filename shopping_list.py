9fe7258a1a135b9654d62a18ed790a25dddf067e
Agata Martyna <agatamartynachalupka@gmail.com>
2020-10-21 22:31:11 +0200

    Add capitals

--- shopping_list.py
+++ shopping_list.py
@@ -4,4 +4,4 @@ shop_list = {
     'kiosk':['gazeta', 'baterie', 'zapałki']
 }
 for item in shop_list:
-    print(f"Idę do {item} i kupuję {shop_list[item]}.")
+    print(f"Idę do {item.capitalize()} i kupuję {', '.join([c.capitalize() for c in shop_list[item]])}.")
